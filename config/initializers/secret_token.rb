3fe80135afa0ca2247478101698ce8111b868bb2
Justin Kronz <jkronz@gmail.com>
2014-01-18 10:12:59 -0500

    Create restaurant scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lunchbot::Application.config.secret_key_base = 'c25c004491b8aec7aa946ffe38816a641cd738d68336645829a417a2c0f891c0a21bf468de080482b3ea308cf46f720cb4b8f8e2131802951fa1296b49299cdd'
